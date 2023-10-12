e05c36e8a18e21993c130cbf073f3c5eb5f50fcd
cswangxiaowei <145024452+cswangxiaowei@users.noreply.github.com>
2023-10-12 13:19:13 -0500

    Update test_ai.py
    We can utilize: Autoagen and Super Agi to solve the issues of GPT engineer.  The following is an example we solved based on Autoagen. (autogen direct run + iterative debug + auto env install) + (gpt-engineer multi-folder improve)

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -7,3 +7,87 @@ from gpt_engineer.core.ai import AI
 def test_ai():
     AI()
     # TODO Assert that methods behave and not only constructor.
+
+#demo for direct run + iteratively debug
+# create an AssistantAgent named "assistant"
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config={
+        "seed": 42,  # seed for caching and reproducibility
+        "config_list": config_list,  # a list of OpenAI API configurations
+        "temperature": 0,  # temperature for sampling
+    },  # configuration for autogen's enhanced inference API which is compatible with OpenAI API
+)
+# create a UserProxyAgent instance named "user_proxy"
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
+)
+# the assistant receives a message from the user_proxy, which contains the task description
+user_proxy.initiate_chat(
+    assistant,
+    message="""give me the solution code in python and execute it for me; (you need to write a more complete test set by yourself, and test correctness; if not debug your code): 4. Median of Two Sorted Arrays
+Hard
+Topics
+Companies
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ 
+The overall run time complexity should be O(log (m+n)).
+ 
+ 
+ 
+Example 1:
+ 
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+ 
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+ 
+Constraints:
+ 
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-10^6 <= nums1[i], nums2[i] <= 10^6""",
+)
+
+#demo for auto env install
+# create an AssistantAgent named "assistant"
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config={
+        "seed": 42,  # seed for caching and reproducibility
+        "config_list": config_list,  # a list of OpenAI API configurations
+        "temperature": 0,  # temperature for sampling
+    },  # configuration for autogen's enhanced inference API which is compatible with OpenAI API
+)
+# create a UserProxyAgent instance named "user_proxy"
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
+)
+# the assistant receives a message from the user_proxy, which contains the task description
+user_proxy.initiate_chat(
+    assistant,
+    message="""Uninstall any existing version of matplotlib, then reinstall it, and create a Python script to plot a sine wave using matplotlib.
+""",
+)
